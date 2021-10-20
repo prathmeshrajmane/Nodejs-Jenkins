966e57c3e4dbb9916a3842f955afe2d3b2b6b0b3
prathmeshrajmane <prathmesh.rajmane1@gmail.com>
2021-10-20 19:26:30 +0530

    edited main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var app = express()
 
 //Define request response in root URL (/)
 app.get('/', function (req, res) {
-  res.send('Hello Welcome to Automateinfra.com')
+  res.send('Hello Welcome to prathmeshrjamane.com')
 })
 
 
